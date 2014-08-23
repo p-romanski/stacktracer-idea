8e343d1b3fd0f878605f6c8e342e1cda39e832e2
Piotr Romański <piotrek.romanski@gmail.com>
2014-08-23 19:22:42 +0200

    Parsing q as file-name:line-no

--- src/ProjectFileSearcher.java
+++ src/ProjectFileSearcher.java
@@ -14,17 +14,39 @@ public class ProjectFileSearcher implements Searcher {
 
     @Override
     public void search(String q) {
-        final PsiFile[] files = PsiShortNamesCache.getInstance(project).getFilesByName(q);
+        String fileName = q;
+        int lineNo = 0;
 
-        ApplicationManager.getApplication().invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                if (files.length > 0) {
-                    OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, files[0].getVirtualFile(), 0, 0);
-                    openFileDescriptor.navigate(true);
-                    WindowManagerEx.getInstance().findVisibleFrame().requestFocus();
-                }
+        if (fileName.contains(":")) {
+            String[] parts = fileName.split(":");
+            fileName = parts[0];
+            try {
+                lineNo = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
             }
-        });
+        }
+
+        PsiFile[] files = PsiShortNamesCache.getInstance(project).getFilesByName(fileName);
+        if (files.length > 0) {
+            ApplicationManager.getApplication().invokeLater(new OpenFile(files[0], lineNo));
+        }
+    }
+
+    private class OpenFile implements Runnable {
+        private final PsiFile file;
+        private final int lineNo;
+
+        public OpenFile(PsiFile file, int lineNo) {
+            this.file = file;
+            this.lineNo = lineNo;
+        }
+
+        @Override
+        public void run() {
+            OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, file.getVirtualFile(), lineNo, 0);
+            openFileDescriptor.navigate(true);
+            WindowManagerEx.getInstance().findVisibleFrame().requestFocus();
+        }
     }
 }
